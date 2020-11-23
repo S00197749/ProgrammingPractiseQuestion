ee861db7bcb46876804d2fae4d5ac2cc1c0d2b0d
S00197749 Luke Sweeney <S00197749@mail.itsligo.ie>
2020-11-23 11:40:50 +0000

    Added Song Class

---
+++ ProgrammingPractiseQuestion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProgrammingPractiseQuestion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
