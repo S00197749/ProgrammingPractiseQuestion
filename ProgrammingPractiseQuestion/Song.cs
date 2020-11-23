46157b27549c2aeaed15933e07929a47c1fffa49
S00197749 Luke Sweeney <S00197749@mail.itsligo.ie>
2020-11-23 11:41:56 +0000

    Added Song Class

---
+++ ProgrammingPractiseQuestion/Song.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingPractiseQuestion
+{
+    class Song
+    {
+        #region Properties
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public double Duration { get; set; }
+
+        public Genre MusicGenre { get; set; }
+        
+        #endregion Properties
+    }
+    public enum Genre { Rock, Pop, Dance, Other}
+}
