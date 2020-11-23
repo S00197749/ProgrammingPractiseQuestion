b61c643492667ca2756b33a6be9052383f696d4f
S00197749 Luke Sweeney <S00197749@mail.itsligo.ie>
2020-11-23 11:55:00 +0000

    Added Properties, Constructors, enum and method to Song Class

--- ProgrammingPractiseQuestion/Song.cs
+++ ProgrammingPractiseQuestion/Song.cs
@@ -12,8 +12,27 @@ namespace ProgrammingPractiseQuestion
         public double Duration { get; set; }
 
         public Genre MusicGenre { get; set; }
-        
+
         #endregion Properties
+
+        #region Constructors
+        public Song (string title, string artist, double duration, Genre musicgenre)
+        {
+            Title = title;
+            Artist = artist;
+            Duration = duration;
+            MusicGenre = musicgenre;
+        }
+        public Song(string title, string artist):this(title, artist, 0, Genre.Other) { }
+
+        public Song() : this("Unknown", "Unknown") { }
+
+        #endregion Constructors
+
+        public override string ToString()
+        {
+            return string.Format($"{Title} {Artist} {Duration} {MusicGenre}");
+        }
     }
     public enum Genre { Rock, Pop, Dance, Other}
 }
